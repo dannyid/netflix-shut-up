2bad36e31a51d1f402abd1a155cd16f67eca9598
Dannyid <danielshekhtman@gmail.com>
2014-04-06 16:49:22 -0400

    trying new code from http://davidwalsh.name/disable-console#comment-113795

--- background.js
+++ background.js
@@ -5,3 +5,22 @@ var options = {
 
 Object.defineProperty(window, 'console', options);
 
+javascript:window.console2 = (function () {
+
+    var globals = [],
+    iframe = document.createElement(‘iframe’),
+    cleanWindow;
+
+    iframe.src = ‘about:blank’;
+    iframe.style.display = ‘none’;
+    document.body.appendChild(iframe);
+
+    return (iframe.contentWindow || iframe.contentDocument).console;
+    
+}());
+
+Object.defineProperty(window, “console”, {
+    get: function () { 
+        return window.console2; 
+    }
+})
